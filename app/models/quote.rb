336a65c85a63040fb3eda67e48abdd96e7dbcce7
Daniel Alves <dalima.dev@gmail.com>
2024-08-27 08:34:41 -0300

    fix turbo security breach on quotes

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -15,5 +15,5 @@ class Quote < ApplicationRecord
   # after_update_commit -> { broadcast_replace_later_to 'quotes' }
 
   # Those first three callbacks are equivalent to the following single line
-  broadcasts_to ->(quote) { 'quotes' }, inserts_by: :prepend
+  broadcasts_to ->(quote) { [quote.company, 'quotes'] }, inserts_by: :prepend
 end
